4d8710114860efbfebd3d41d0b43773bd46a87d2
Michele Segata <segata@ccs-labs.org>
2017-06-16 08:43:49 +0200

    make non interactive plot visualize simulation's evolution using playwith

--- plot-mpc.R
+++ plot-mpc.R
@@ -1,3 +1,7 @@
+if (!interactive()) {
+	library(playwith)
+}
+
 args <- commandArgs(trailingOnly=T)
 
 csv.file = 'res.csv'
@@ -25,37 +29,54 @@ if (!interactive()) {
     par(mar=c(3.2, 3.2, 0.3, 0.3))
 }
 
-# create a plot
-plot.new()
-plot.window(xlim=c(0, max(d$time)), ylim=c(-1.6, 1.6), xaxs="i", yaxs="i")
-
-lines(d$time, d$v, lwd=2, col=1, lty=1)
-abline(h=1, lwd=2, col=2, lty=2)
-lines(d$time, d$a, lwd=2, col=3, lty=3)
-lines(d$time, d$u, lwd=2, col=4, lty=4)
-lines(d$time, d$du, lwd=2, col=5, lty=5)
-if (length(hlines) > 0) {
-    for (h in hlines)
-        abline(h=h, lwd=2, col=gray(0.5), lty=2)
+dataset <- function(ns) {
+    if (ns == -1) {
+        subset(d, k == 1)
+    } else {
+        subset(d, n == ns)
+    }
 }
 
-legend("bottomright",
-       c('speed (m/s)', 'reference (m/s)', 'acceleration (m/s/s)', 'control (m/s/s)', 'control derivative (m/s/s/s)'),
-       ncol=1,
-       inset=c(0, 0),
-       lty=1:5,
-       col=1:5,
-       lwd=2,
-       box.lwd=0,
-       pch=NA,
-       pt.bg='white',
-       cex=.8,
-       seg.len=3)
+plot.graph <- function(ds=dataset(-1), xlim=c(min(ds$time), max(ds$time)), ylim=c(-1.6, 1.6)) {
 
-title(xlab='time (s)')
-axis(1, las=1)
-axis(2, las=1)
-box()
+    # create a plot
+    plot.new()
+    plot.window(xlim=xlim, ylim=ylim, xaxs="i", yaxs="i")
 
-if (!interactive())
-    dev.off()
+    lines(ds$time, ds$v, lwd=2, col=1, lty=1)
+    abline(h=1, lwd=2, col=2, lty=2)
+    lines(ds$time, ds$a, lwd=2, col=3, lty=3)
+    lines(ds$time, ds$u, lwd=2, col=4, lty=4)
+    lines(ds$time, ds$du, lwd=2, col=5, lty=5)
+    if (length(hlines) > 0) {
+        for (h in hlines)
+            abline(h=h, lwd=2, col=gray(0.5), lty=2)
+    }
+
+    legend("bottomright",
+           c('speed (m/s)', 'reference (m/s)', 'acceleration (m/s/s)', 'control (m/s/s)', 'control derivative (m/s/s/s)'),
+           ncol=1,
+           inset=c(0, 0),
+           lty=1:5,
+           col=1:5,
+           lwd=2,
+           box.lwd=0,
+           pch=NA,
+           pt.bg='white',
+           cex=.8,
+           seg.len=3)
+
+    title(xlab='time (s)')
+    axis(1, las=1)
+    axis(2, las=1)
+    box()
+
+    if (!interactive())
+        dev.off()
+}
+
+if (!interactive()) {
+    plot.graph(ds=dataset(0))
+} else {
+    playwith(plot.graph(ds=dataset(st)), parameters=list("st" = c(-1, max(d$n))))
+}
